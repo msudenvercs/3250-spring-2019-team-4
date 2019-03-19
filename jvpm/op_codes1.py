30baab92ad992d4f46bfb6c0183f5e28c4412cde
imutahi <imutahi@msudenver.edu>
2019-03-18 18:33:49 -0600

    ith '#' will be ignored, and an empty message aborts the commit.

--- jvpm/op_codes1.py
+++ jvpm/op_codes1.py
@@ -216,3 +216,26 @@ class op_codes:
                 else:
                         stack_z.append(varsarray[index])
                         return stack_z
+
+        def op_code36(stack_z, varsarray, index):
+                if (index > len(varsarray)):
+                        raise IndexError
+                else:
+                        stack_z.append(varsarray[index])
+                        return stack_z
+
+        def op_code3b(stack_z, varsarray):
+                        stack_z.append(varsarray[0])
+                        return stack_z
+
+        def op_code3c(stack_z, varsarray):
+                        stack_z.append(varsarray[1])
+                        return stack_z
+
+        def op_code3d(stack_z, varsarray):
+                        stack_z.append(varsarray[2])
+                        return stack_z
+
+        def op_code3e(stack_z, varsarray):
+                        stack_z.append(varsarray[3])
+                        return stack_z
