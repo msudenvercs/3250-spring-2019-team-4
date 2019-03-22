240f41e3d93d54637197aa9b453a05ed78b98483
snubeagle <rmccull7@msudenver.edu>
2019-03-22 09:33:00 -0600

    fixed some spacing issues

--- jvpm/test/test_op_codes.py
+++ jvpm/test/test_op_codes.py
@@ -5,37 +5,37 @@ from jvpm import op_codes1
 class test_op_codes(unittest.TestCase):
 
     def test_opcode05(self): #load 2 onto stack
-        test_stack = [1,2]
+        test_stack = [1, 2]
         test_stack = op_codes1.op_codes.op_code05(test_stack)
         self.assertEqual(test_stack.pop(), 2)
 
     def test_opcode06(self): #load 3 onto stack
-        test_stack = [1,2]
+        test_stack = [1, 2]
         test_stack = op_codes1.op_codes.op_code06(test_stack)
         self.assertEqual(test_stack.pop(), 3)
 
     def test_opcode07(self): #load 4 onto stack
-        test_stack = [1,2]
+        test_stack = [1, 2]
         test_stack = op_codes1.op_codes.op_code07(test_stack)
         self.assertEqual(test_stack.pop(), 4)
 
     def test_opcode08(self): #load 5 onto stack
-        test_stack = [1,2]
+        test_stack = [1, 2]
         test_stack = op_codes1.op_codes.op_code08(test_stack)
         self.assertEqual(test_stack.pop(), 5)
 
     def test_op_code70(self): # remainder
-        test_stack = [1,2]
+        test_stack = [1, 2]
         test_stack = op_codes1.op_codes.op_code70(test_stack)
         self.assertEqual(test_stack.pop() , 0)
 
     def test_op_code78(self): # shift left
-        test_stack = [1,2]
+        test_stack = [1, 2]
         test_stack = op_codes1.op_codes.op_code78(test_stack)
         self.assertEqual(test_stack.pop() , 4)
 
     def test_op_code7c(self): #shift right
-        test_stack = [1,2]
+        test_stack = [1, 2]
         test_stack = op_codes1.op_codes.op_code7c(test_stack)
         self.assertEqual(test_stack.pop() , 1)
 
@@ -74,12 +74,12 @@ class test_op_codes(unittest.TestCase):
         self.assertEqual(test_stack.pop(), 1)
 
     def test_op_code7e(self): # bitwise AND
-        test_stack = [1,2]
+        test_stack = [1, 2]
         test_stack = op_codes1.op_codes.op_code7e(test_stack)
         self.assertEqual(test_stack.pop(), 1&2)
 
     def test_op_code6c(self): # integer division
-        test_stack = [1,2,2,0]
+        test_stack = [1, 2, 2, 0]
         self.assertRaises(ArithmeticError, test_stack = op_codes1.op_codes.op_code6c(test_stack))
         test_stack = op_codes1.op_codes.op_code6c(test_stack)
         self.assertEqual(test_stack.pop(), 1 // 2)
@@ -102,17 +102,17 @@ class test_op_codes(unittest.TestCase):
         self.assertEqual(test_stack.pop(), 1 * 2)
 
     def test_op_code74(self): # negative
-        test_stack = [1,2]
+        test_stack = [1, 2]
         test_stack = op_codes1.op_codes.op_code74(test_stack)
         self.assertEqual(test_stack.pop() , -1 * 2)
 
     def test_opcode02(self): #load -1 onto stack
-        test_stack = [1,2]
+        test_stack = [1, 2]
         test_stack = op_codes1.op_codes.op_code02(test_stack)
         self.assertEqual(test_stack.pop(), -1)
 
     def test_opcode03(self): #load 0 onto stack
-        test_stack = [1,2]
+        test_stack = [1, 2]
         test_stack = op_codes1.op_codes.op_code03(test_stack)
         self.assertEqual(test_stack.pop(), 0)
 
