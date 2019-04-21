192798490179ee9d5184ab280918d2ca8e12bea8
KenjiMorizono <kenji.morizono1@gmail.com>
2019-04-21 08:06:05 -0600

    invokevirtual call for nextInt is completed

--- jvpm/java/util/Scanner.py
+++ jvpm/java/util/Scanner.py
@@ -1,18 +1,6 @@
-import os
-dir = ""
-
-
-class Scanner:
-    def nextInt(self):
-        try:
-            user_input = int(input())
-        except ValueError:
-            raise TypeError("java.util.InputMismatchException")
-
-    def __init__(self):
-        # TODO Figure out a nice way to imitate the exception throwing path, in java it shows java.util.InputMismatchException
-        print("TODO")
-
-
-a = Scanner()
-a.nextInt()
+def nextInt():
+    try:
+        user_input = int(input())
+        return user_input
+    except ValueError:
+        raise TypeError("java.util.InputMismatchException")
