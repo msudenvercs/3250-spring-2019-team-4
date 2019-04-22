d8e039c5f6bcc62fb29b3f8b00d16c0372881702
GeromanEmpire <47043651+GeromanEmpire@users.noreply.github.com>
2019-04-22 11:12:55 -0600

    Fixed op_code parameters.

--- jvpm/op_codes1.py
+++ jvpm/op_codes1.py
@@ -294,20 +294,20 @@ class op_codes:
                         raise IndexError
 
         #opcodes moved from ClassFile.py
-        def op_codeb2(opcodes, opcode):
+        def op_codeb2(self, opcodes, opcode):
             pool_index = opcodes.index(opcode)
             code_index = int("".join(map(str, opcodes[pool_index+1:pool_index+3])),16)
             self.recursive(code_index-1)
 
-        def op_codeb1(opcodes, opcode):
+        def op_codeb1(self, opcodes, opcode):
             return None
 
-        def op_code10(opcodes, opcode):
+        def op_code10(self, opcodes, opcode):
             pool_index = opcodes.index(opcode)
             constant = int("".join(map(str, opcodes[pool_index+2:pool_index+3])),16)
             self.stack_z.append(constant)
 
-        def op_code12(opcodes, opcode):
+        def op_code12(self, opcodes, opcode):
             pool_index = opcodes.index(opcode)
             table_index = int("".join(map(str, opcodes[pool_index+1:pool_index+2])),16)
             string = self.formatted_constant_table[table_index][1]
