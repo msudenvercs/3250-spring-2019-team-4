f6257af8dfa169c040134574adc961aab82581fb
imutahi <imutahi@msudenver.edu>
2019-05-10 18:42:25 -0600

    Update op_codes1.py
    Fixed all of the previous mistakes, and added self to arguments

--- jvpm/op_codes1.py
+++ jvpm/op_codes1.py
@@ -105,7 +105,7 @@ class op_codes:
         stack_z.append(var1)
         return stack_z
 
-    def isub(self):  # int subtract
+    def isub(self, stack):  # int subtract
         # Assumes values put on the stack have already been converted to decimal integers
         x = stack.pop()
         y = stack.pop()
@@ -114,12 +114,12 @@ class op_codes:
         stack.append(result)
         return stack
 
-    def iand(self):  # bitwise and for integers
+    def iand(self, stack_z):  # bitwise and for integers
         var1 = stack_z.pop() & stack_z.pop()
         stack_z.append(var1)
         return stack_z
 
-    def idiv(self):  # integer division
+    def idiv(self, stack_z):  # integer division
         var1 = stack_z.pop()
         var2 = stack_z.pop()
 
@@ -131,7 +131,7 @@ class op_codes:
         stack_z.append(var1)
         return stack_z
 
-    def imul(self):  # int multiplication
+    def imul(self, stack_z):  # int multiplication
         MAX_JAVA_INT = 2147483647
         MIN_JAVA_INT = -2147483647
         var1 = stack_z.pop()
@@ -169,24 +169,24 @@ class op_codes:
         stack_z.append(var1)
         return stack_z
 
-    def ineg(self):  # change int to negative
+    def ineg(self, stack_z):  # change int to negative
         var1 = stack_z.pop() * -1
         stack_z.append(var1)
         return stack_z
 
-    def iconst_m1(self):  # loads int -1 into the stack
+    def iconst_m1(self, stack_z):  # loads int -1 into the stack
         stack_z.append(-1)
         return stack_z
 
-    def iconst_0(self):  # loads int 0 into the stack
+    def iconst_0(self, stack_z):  # loads int 0 into the stack
         stack_z.append(0)
         return stack_z
 
-    def iconst_1(self):  # loads 1 into the stack
+    def iconst_1(self, stack_z):  # loads 1 into the stack
         stack_z.append(1)
         return stack_z
 
-    def i2c(self):  # converts int to character
+    def i2c(self, stack_z):  # converts int to character
         var1 = stack_z.pop()
         if var1 >= 32 & var1 <= 127:
             stack_z.append(chr(var1))
@@ -198,38 +198,38 @@ class op_codes:
         stack_z.append(float(stack_z.pop()))
         return stack_z
 
-    def i2f(self):  #converts int to float
+    def i2f(stack_z):  #converts int to float
         return op_codes.i2d(stack_z)
 
-    def i2l(self):  #converts int to long
+    def i2l(self, stack_z):  #converts int to long
         var1 = stack_z.pop()
         stack_z.append(int(var1))
         return stack_z
 
-    def iload_0(stack_z, varsarray):  # load int value from local var 0
+    def iload_0(self, stack_z, varsarray):  # load int value from local var 0
         stack_z.append(varsarray[0])
         return stack_z
 
-    def iload_1(stack_z, varsarray):  # load int value from local var 1
+    def iload_1(self, stack_z, varsarray):  # load int value from local var 1
         stack_z.append(varsarray[1])
         return stack_z
 
-    def iload_2(stack_z, varsarray):  # load int value from local var 2
+    def iload_2(self, stack_z, varsarray):  # load int value from local var 2
         stack_z.append(varsarray[2])
         return stack_z
 
-    def iload_3(stack_z, varsarray):  # load int value from local var 3
+    def iload_3(self, stack_z, varsarray):  # load int value from local var 3
         stack_z.append(varsarray[3])
         return stack_z
 
-    def iload(stack_z, varsarray, index): # load int value from local var index
+    def iload(self, stack_z, varsarray, index): # load int value from local var index
         if index > len(varsarray):
             raise IndexError
         else:
             stack_z.append(varsarray[index])
             return stack_z
 
-    def i2s( stack_z):  # int to short
+    def i2s(self, stack_z):  # int to short
         MAX_JAVA_INT = 2147483647
         MIN_JAVA_INT = -2147483647
         var1 = stack_z.pop()
@@ -255,7 +255,7 @@ class op_codes:
         stack_z.append(var1)
         return stack_z
 
-    def iaload( array, index):  # load an int from an array
+    def iaload(self, array, index):  # load an int from an array
         if index < len(array):
             stack_z = []
             stack_z.append(array[index])
@@ -263,27 +263,27 @@ class op_codes:
         else:
             raise IndexError
 
-    def long_builder(hex):
+    def long_builder(self, hex):
         return int(numpy.binary_repr(int(hex, 16), width=64), 2)
 
-    def op_code16(hex):
+    def op_code16(self, hex):
         stack_z = []
         stack_z.append(op_codes.long_builder(hex))
         return stack_z
 
-    def op_code1e(stack_z, local_vars):
+    def op_code1e(self, stack_z, local_vars):
         stack_z = op_codes.op_code16(local_vars[0])
         return stack_z
 
-    def op_code1f(stack_z, local_vars):
+    def op_code1f(self, stack_z, local_vars):
         stack_z = op_codes.op_code16(local_vars[1])
         return stack_z
 
-    def op_code20(stack_z, local_vars):
+    def op_code20(self, stack_z, local_vars):
         stack_z = op_codes.op_code16(local_vars[2])
         return stack_z
 
-    def op_code21(stack_z, local_vars):
+    def op_code21(self, stack_z, local_vars):
         stack_z = op_codes.op_code16(local_vars[3])
         return stack_z
 
